097e02f0c423c9412edfa208ec0cad98d7d8532e
warren <onsenkame@gmail.com>
2023-11-15 15:55:42 -0800

    added react-hook-form and zod form data

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -1,19 +1,84 @@
 "use client";
 
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
 import { Modal } from "@/components/ui/modal";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Form, FormField, FormItem, FormLabel, FormControl, FormMessage } from "@/components/ui/form";
 import { useStoreModal } from "@/hooks/use-store-modal";
+
+const formSchema = z.object({
+    name: z.string().min(1),
+    description: z.string(),
+});
 
 export const StoreModal = () => {
     const storeModal = useStoreModal();
 
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: "",
+            description: "",
+        },
+    });
+
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        console.log(values, "created from store");
+    };
+
     return (
-    <Modal
-        title="Create Store"
-        description="Add a new store to your account. Manage products and categories here."
-        isOpen={storeModal.isOpen}
-        onClose={storeModal.onClose}
-    >
-        Will be create store form here
-    </Modal>
+        <Modal
+            title="Create Store"
+            description="Add a new store to your account. Manage products and categories here."
+            isOpen={storeModal.isOpen}
+            onClose={storeModal.onClose}
+        >
+            <div className="space-y-4 py-2 pb-4">
+                <div className="space-y-2">
+                    <Form {...form}>
+                        <form onSubmit={form.handleSubmit(onSubmit)}>
+                            <FormField
+                                control={form.control}
+                                name="name"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Name</FormLabel>
+                                        <FormControl>
+                                            <Input placeholder="Store Name Here" {...field} />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <div className="pt-4 space-x-2 w-full">
+                            </div>
+                            <FormField
+                                control={form.control}
+                                name="description"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Description</FormLabel>
+                                        <FormControl>
+                                            <Input placeholder="What are you selling?" {...field} />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <div className="pt-6 space-x-2 flex items-center justify-end w-full">
+                                <Button variant="outline" onClick={storeModal.onClose}>
+                                    Cancel
+                                </Button>
+                                <Button type="submit">Continue</Button>
+                            </div>
+                        </form>
+                    </Form>
+                </div>
+            </div>
+        </Modal>
     );
 };
